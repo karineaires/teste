dc92d83f1ab293520fb82eebabaff7ee89c523a0
karineaires <karineaires@gmail.com>
2021-01-25 12:11:29 -0300

    3 dev

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function index() {
   return (
     <View>
-      <Text>exemplo 2</Text>
+      <Text>exemplo 3 dev</Text>
     </View>
   );
 }
